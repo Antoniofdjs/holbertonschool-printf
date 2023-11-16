7e277ed7ad7336e857374adc73a38402b59d6a19
Antoniofdjs <Antoniofdjs@gmail.com>
2023-11-16 16:12:53 -0400

    New version print

--- my_print.c
+++ my_print.c
@@ -1,110 +1,40 @@
 #include <stdio.h>
 #include "main.h"
 #include "string.h"
+#include <unistd.h>
 #include "stdarg.h"
 
-/**
- * print_c - prints chars
- * @my_args: pointer to args
- */
-void print_c(va_list *my_args)
-{
-	printf("%c", va_arg(*my_args, int));
-}
-/**
- * print_i - prints ints
- * @my_args: pointer to args
- */
-void print_i(va_list *my_args)
-{
-	printf("%d", va_arg(*my_args, int));
-}
-/**
- * print_f - prints floats
- * @my_args: pointer to args
- */
-void print_f(va_list *my_args)
-{
-	printf("%f", va_arg(*my_args, double));
-	}
-/**
- * print_s - prints strings
- * @my_args: pointer to args
- */
-void print_s(va_list *my_args)
-{
-	char *current_str;
-
-	current_str = va_arg(*my_args, char *);
-	if (current_str == NULL)
-		current_str = "(nil)";
-	printf("%s", current_str);
-}
-/**
- * print_all - prints everything
- * @format: constant format string
- *
- * Return: void
- */
 int _printf(const char *format, ...)
 {
-	int arg_count = 0;/*count for format args*/
-	int i = 0, j = 0;
-	va_list my_args;
-	data my_data[] = {
-		{"c", print_c},
-		{"i", print_i},
-		{"f", print_f},
-		{"s", print_s},
-		{NULL, NULL} /*letter-type, (function*) */
-	};/*Struct char *type, void (*f)()*/
-
-	va_start(my_args, format);
-
+	int case_count = 0;/*on/off switch for cases*/
+	int i = 0;
 
 	while (format != NULL && format[i] != '\0')
 	{
 		if (format[i] == '%')
-		{
+		{	case_count = 0;
 			i++;
 			switch (format[i])/*falta add los functions calls para printear los cases*/
 			{
 				case 'c':
-					arg_count++;
+					printf("-PRINTC-");
+					case_count++;
 					break;
 				case 's':
-					arg_count++;
+					printf("-PRINTS-");
+					case_count++;
 					break;
-				case 'd':
-					arg_count++;
+				case '%':
+					write(1, &format[i], 1);
+					case_count++;
 					break;
+				default:
+					i--;
 			}
 		}
-		i++;
-	};
-	if (arg_count == 0)/* If there are no % cases*/
-	{
-	for (i = 0; format[i] != '\0'; i++)
-			putchar(format[i]);
-		return (0);
-	};/*Codigo hasta aqui------------------------*/
-	i = 0;
-	while (format != NULL && format[i] != '\0') /*ESTA PARTE TODAVIA ARREGLANDO*/
-	{
-		j = 0;
-		while (my_data[j].f != NULL)/*still have functions to call*/
-		{
-			if (*my_data[j].type == format[i])/*match letters = data type */
-			{
-				my_data[j].f(my_args);/*call matching print function */
-				break;
-			}
-			j++;
-		}
+		if (case_count == 0)
+			write(1, &format[i], 1);
 		i++;
 	}
-
-	va_end(my_args);
-	printf("\n");
 	return (0);
 }
