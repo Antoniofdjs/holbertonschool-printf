06133b96b593500876e19e24a65668126e03f2f4
Antoniofdjs <Antoniofdjs@gmail.com>
2023-11-19 16:00:51 -0400

    Changed struct and changed write_x proto

--- main.h
+++ main.h
@@ -2,13 +2,13 @@
 #define MAIN_H
 typedef struct {
 	char *type;
-	int (*f)(va_list *);
+	int (*f)();
 }data;
 int _printf(const char *format, ...);
 int my_write_cs(va_list *my_args);
 int write_s(va_list *my_args);
 int write_c(va_list *my_args);
 int write_d(va_list *my_args);
-int write_x(va_list *my_args);
+int write_x(va_list *my_args, const char *current_letter);
 int write_p(va_list *my_args);
 #endif
