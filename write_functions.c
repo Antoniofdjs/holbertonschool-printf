2196e2491bd2350899a675a338a34c52a41aa57c
Antoniofdjs <Antoniofdjs@gmail.com>
2023-11-19 17:36:43 -0400

    Adding comments, fixing betty errors, still missing comments

--- write_functions.c
+++ write_functions.c
@@ -6,44 +6,69 @@
 #include "stdlib.h"
 #include <stdint.h>
 
-int write_s(va_list *my_args) {
+/**
+ * write_s - function for writing strings from a list
+ * @my_args: my va_list
+ * Return: total count of characters
+ */
+
+int write_s(va_list *my_args)
+{
 	char *current_str;
 	int j = 0, count = 0;
 
 	current_str = va_arg(*my_args, char *);
-	if (current_str == NULL) {
-		write(1,"(NILL)", 6);
+	if (current_str == NULL)
+	{
+		write(1, "(NILL)", 6);
 		return (0);
 	}
-	while (current_str[j] != '\0') {
+	while (current_str[j] != '\0')
+	{
 		write(1, &current_str[j], 1);
 		j++, count++;
 	}
 	return (count);
 }
 
-int write_c(va_list *my_args) {
+/**
+ * write_c - writes single chars from list
+ * @my_args: my va_list
+ * Return: total count of characters
+ */
+
+int write_c(va_list *my_args)
+{
 	char current_char;
+
 	current_char = va_arg(*my_args, int);
 
 	write(1, &current_char, 1);
-
 	return (1);
 }
 
-int write_d(va_list *my_args) {
+/**
+ * write_d - writes ints or decimal from list
+ * @my_args: my va_list
+ * Return: total count of characters
+ */
+
+int write_d(va_list *my_args)
+{
 	int temp, result, digits, count = 0, i;
 	char *str;
-	
+
 	result = va_arg(*my_args, int);
-	if (result < 0) {
+	if (result < 0)
+	{
 		result = -result;
 		write(1, "-", 1);
 		count++;
 	}
 	temp = result;
 	digits = 0;
-	while (temp != 0) {/*count the number of digits*/
+	while (temp != 0)/*count the number of digits*/
+	{
 		temp /= 10;
 		digits++;
 	}
@@ -52,11 +77,13 @@ int write_d(va_list *my_args) {
 		return (-1);
 	if (str == NULL)
 		exit(98);
-	for (i = digits - 1; i >=0; i--) {/* storing nums in str in reverse */
+	for (i = digits - 1; i >= 0; i--)/* storing nums in str in reverse */
+	{
 		str[i] = '0' + (result % 10);
 		result /= 10;
 	}
-	for (i = 0; i < digits; i++) {
+	for (i = 0; i < digits; i++)
+	{
 		write(1, &str[i], 1);
 		count++;
 	}
@@ -64,30 +91,41 @@ int write_d(va_list *my_args) {
 	return (count);
 }
 
-int write_x(va_list *my_args, const char *format) {
+/**
+ * write_x - convert decimals to hex & writes them from va_list
+ * @my_args: my va_list
+ * @format: char from my_printf,contains x or X
+ * Return: total count of characters
+ */
+
+int write_x(va_list *my_args, const char *format)
+{
 	unsigned int result = 0, count = 0;
 	char *str;
 	int digits = 0, temp, i, is_low = 0;
 
 	if (*format == 'x')
-		is_low = 32;
+		is_low = 32;/*variable changes to lower cases if needed */
 	result = va_arg(*my_args, int);
 	temp = result;
-	while (temp != 0) {/*count the number of digits*/
+	while (temp != 0)/*count the number of digits*/
+	{
 		temp /= 16;
 		digits++;
 	}
 	str = malloc(sizeof(char) * (digits + 1));
 	if (str == NULL)
 		exit(98);
-	for (i = digits - 1; i >= 0; i--) { /* storing nums in str in reverse */
+	for (i = digits - 1; i >= 0; i--)/* storing nums in str in reverse */
+	{
 		if ((result % 16 > 9))/* 10 - 16 cases */
 			str[i] = 'A' + is_low + ((result % 16) - 10);/* example 11 = a + 1 = b */
 		else
 			str[i] = ('0' + result % 16); /* 0 - 9 cases */
 		result /= 16;
 	}
-	for (i = 0; i < digits; i++) {
+	for (i = 0; i < digits; i++)
+	{
 		write(1, &str[i], 1);
 		count++;
 	}
@@ -95,7 +133,14 @@ int write_x(va_list *my_args, const char *format) {
 	return (count + 2);
 }
 
-int write_p(va_list *my_args) {
+/**
+ * write_p ->>>>>COMMENT HERE LOUIS<<<<
+ * @my_args: my va_list
+ * Return: total count of characters
+ */
+
+int write_p(va_list *my_args)
+{
 	void *ptr = va_arg(*my_args, void *);
 	uintptr_t addr = (uintptr_t)ptr;
 	uintptr_t temp;
@@ -103,31 +148,34 @@ int write_p(va_list *my_args) {
 	char *str;
 	int digits = 0, i;
 
-	if (addr == 0) {
+	if (addr == 0)
+	{
 		write(1, "(nil", 5);
-		return(5);
+		return (5);
 	}
 	temp = addr;
-	while (temp != 0) {/*calculate the number of digits*/
+	while (temp != 0)/*calculate the number of digits*/
+	{
 		temp /= 16;
 		digits++;
 	}
 	str = malloc(sizeof(char) * (digits + 3));
-	if (str == NULL) {
+	if (str == NULL)
 		exit(98);
-	}
-	str[0] = '0';
+
+	str[0] = '0';/* >>>>COMMENT HERE LOUIS<<<< */
 	str[1] = 'x';
-	for (i = digits + 1; i >= 2; i--) {
-		if (addr % 16 > 9) {
-			str[i] = 'a' + (addr % 16- 10);
-		} else {
+	for (i = digits + 1; i >= 2; i--)
+	{
+		if (addr % 16 > 9)
+			str[i] = 'a' + (addr % 16 - 10);
+		else
 			str[i] = '0' + addr % 16;
-		}
-		addr /= 16;
+	addr /= 16;
 	}
-	str[digits + 2] = '\0';
-	for (i = 0; i < digits + 2; i++) {
+	str[digits + 2] = '\0';/* >>> COMMENT HERE LOUIS<<< */
+	for (i = 0; i < digits + 2; i++)
+	{
 		write(1, &str[i], 1);
 		count++;
 	}
