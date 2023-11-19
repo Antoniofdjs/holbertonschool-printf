51177b9b1db13d68234681e234f305240e5becdd
Louis Toro <louistororosario@gmail.com>
2023-11-19 12:09:32 -0400

    write_p function added, made some chanhges(delete empty lines)

--- write_functions.c
+++ write_functions.c
@@ -4,32 +4,25 @@
 #include <unistd.h>
 #include "stdarg.h"
 #include "stdlib.h"
+#include <stdint.h>
 
-int write_s(va_list *my_args)
-{
-
+int write_s(va_list *my_args) {
 	char *current_str;
-	int j = 0;
-	int count = 0;
+	int j = 0, count = 0;
 
 	current_str = va_arg(*my_args, char *);
-	if (current_str == NULL)
-	{
+	if (current_str == NULL) {
 		write(1,"(NILL)", 6);
 		return (0);
 	}
-	while (current_str[j] != '\0')
-	{
+	while (current_str[j] != '\0') {
 		write(1, &current_str[j], 1);
-		j++;
-		count++;
+		j++, count++;
 	}
-
 	return (count);
 }
 
-int write_c(va_list *my_args)
-{
+int write_c(va_list *my_args) {
 	char current_char;
 	current_char = va_arg(*my_args, int);
 
@@ -38,53 +31,101 @@ int write_c(va_list *my_args)
 	return (1);
 }
 
-int write_d(va_list *my_args)
-{
-	int result, count = 0, i;
+int write_d(va_list *my_args) {
+	int temp, result, digits, count = 0, i;
 	char *str;
-	char digit;
-	int digits;
-	int temp;
 	
 	result = va_arg(*my_args, int);
-
-	if (result < 0)
-	{
+	if (result < 0) {
 		result = -result;
 		write(1, "-", 1);
 		count++;
 	}
-	if (result <= 9)/* only one digit coming */
-	{
-		digit = ('0' + result);
-		write(1, &digit, 1);
-		count++;
-		return (count);
-	}
 	temp = result;
 	digits = 0;
-
-	while (temp != 0)/*count the number of digits*/
-	{
+	while (temp != 0) {/*count the number of digits*/
 		temp /= 10;
 		digits++;
 	}
 	str = malloc(sizeof(char) * (digits + 1));
-	if (str == NULL)
-	{
+	if (str == NULL) {
 		return (-1);
 	}
-	for (i = digits - 1; i >=0; i--)/* storing nums in str in reverse */
-	{
+	for (i = digits - 1; i >=0; i--) {/* storing nums in str in reverse */
 		str[i] = '0' + (result % 10);
 		result /= 10;
 	}
-	for (i = 0; i < digits; i++)
-	{
+	for (i = 0; i < digits; i++) {
 		write(1, &str[i], 1);
 		count++;
 	}
+	free(str);
+	return (count);
+}
 
+int write_x(va_list *my_args) {
+	unsigned int result = 0, count = 0;
+	char *str;
+	int digits = 0, temp, i;
+
+	result = va_arg(*my_args, int);
+	temp = result;
+	while (temp != 0) {/*count the number of digits*/
+		temp /= 16;
+		digits++;
+	}
+	str = malloc(sizeof(char) * (digits + 1));
+	if (str == NULL)
+		exit(98);
+	for (i = digits - 1; i >= 0; i--) { /* storing nums in str in reverse */
+		if ((result % 16 > 9))/* 10 - 16 cases */
+			str[i] = 'a' + ((result % 16) - 10);/* example 11 = a + 1 = b */
+		else
+			str[i] = ('0' + result % 16); /* 0 - 9 cases */
+		result /= 16;
+	}
+	for (i = 0; i < digits; i++) {
+		write(1, &str[i], 1);
+		count++;
+	}
+	free(str);
+	return (count + 2);
+}
+
+int write_p(va_list *my_args) {
+	void *ptr = va_arg(*my_args, void *);
+	uintptr_t addr = (uintptr_t)ptr;
+
+	unsigned int count = 0;
+	char *str;
+	int digits = 0, i;
+
+	if (addr == 0) {
+		write(1, "(nil)", 5);
+		return (5);
+	}
+	while (addr == 0) {
+		addr /= 16;
+		digits++;
+	}
+	str = malloc(sizeof(char) * (digits + 3));
+	if (str == NULL) {
+		exit(98);
+	}
+	str[0] = '0';
+	str[1] = 'x';
+	for (i = digits + 1; i >= 2; i--) {
+		if (addr % 16 > 9) {
+			str[i] = 'a' + (addr % 16- 10);
+		} else {
+			str[i] = '0' + addr % 16;
+		}
+		addr /= 16;
+	}
+	for (i = 0; i < digits + 2; i++) {
+		write(1, &str[i], 1);
+		count++;
+	}
 	free(str);
 	return (count);
 }
